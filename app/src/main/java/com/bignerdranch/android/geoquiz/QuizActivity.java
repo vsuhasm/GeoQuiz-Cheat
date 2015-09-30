4beb56606909682588ea913aa20f21cfc0ff1b25
vsuhasm <vsuhas.m@gmail.com>
2015-09-29 22:43:15 -0400

    Variable mIsCheater needs to be stored in the state bundle before the actvity is destryed because of rotation.

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -20,6 +20,7 @@ public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
     private static final int REQUEST_CODE_CHEAT = 0;
+    private static final String CHEAT_STATE = "isCheater";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -123,15 +124,18 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(CHEAT_STATE, false);
         }
 
         updateQuestion();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(KEY_INDEX, mCurrentIndex);
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState() called");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(CHEAT_STATE, mIsCheater);
     }
 
     @Override
